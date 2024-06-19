aed3568373126f531dd4eb8cc2c7f87887262316
Marcin <marcinbartnicki2@wp.pl>
2024-06-19 11:08:17 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -11,6 +11,7 @@ function getRandomHexColor() {
 }
 
 function createBoxes(amount) {
+  destroyBoxes();
   for (let i = 0; i < amount; i++) {
     const size = 30 + i * 10;
     const box = document.createElement('div');
